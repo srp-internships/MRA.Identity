8b09ed03e97da5db0dc0f9e88b76afae1cae6de6
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-01-26 14:00:04 +0500

    Add Extensions

---
+++ src/MRA.Identity.Client/Services/HttpClientService/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Json;
+
+namespace MRA.Identity.Client.Services.HttpClientService
+{
+    public static class Extensions
+    {
+        public static async Task<T> GetFromJsonAsync<T>(this HttpClient httpClient, string route, object query)
+        {
+            string queryString = string.Join("&", query.GetType().GetProperties()
+                .Select(property => $"{property.Name}={property.GetValue(query)}"));
+            return await httpClient.GetFromJsonAsync<T>($"{route}?{queryString}");
+
+        }
+    }
+}

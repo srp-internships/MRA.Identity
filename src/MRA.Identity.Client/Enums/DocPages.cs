4e1474d7104a1e0946206d0476288c240e99b780
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-29 14:14:01 +0500

    fix dockPages.cs

--- src/MRA.Identity.Client/Enums/DocPages.cs
+++ src/MRA.Identity.Client/Enums/DocPages.cs
@@ -1,9 +1,26 @@
 ﻿namespace MRA.Identity.Client.Enums;
 
-public enum DocPages
+public static class DocPages
 {
-    Home,
-    Academy,
-    Profile,
-    UserManager
+    public const string Home = "home";
+    public const string Jobs = "jobs";
+    public const string Internships = "internships";
+    public const string Trainings = "trainings";
+    public const string Team = "team";
+    public const string Academy = "academy";
+    public const string Contact = "contact";
+    public const string Profile = "profile";
+    public const string Applications = "applications";
+    public const string NoVacancyUploadCv = "upload-cv";
+    public const string Categories = "categories";
+    public const string UserManager = "userManager";
+
+    public static string GetPageFromUrl(string url, bool setDefault = false)
+    {
+        var allPages = typeof(DocPages).GetFields().Select(s => s.GetValue(null) as string);
+        var result = allPages.FirstOrDefault(s => url.EndsWith(s, StringComparison.OrdinalIgnoreCase) || url.EndsWith($"dashboard/{s}", StringComparison.OrdinalIgnoreCase));
+        if (setDefault && result == null)
+            result = Home;
+        return result;
+    }
 }

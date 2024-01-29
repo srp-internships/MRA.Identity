6a81a80b2cf0aca40ea078012081899e58043797
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-01-29 14:10:38 +0500

    commit

--- src/MRA.Identity.Client/Enums/DocPages.cs
+++ src/MRA.Identity.Client/Enums/DocPages.cs
@@ -3,23 +3,7 @@
 public enum DocPages
 {
     Home,
-    Jobs,
-    Internships,
-    Trainings,
-    Team,
     Academy,
-    Contact,
     Profile,
-    Applications,
-    NoVacancyUploadCv
-}
-
-    public static string GetPageFromUrl(string url, bool setDefault = false)
-    {
-        var allPages = typeof(DocPages).GetFields().Select(s => s.GetValue(null) as string);
-        var result = allPages.FirstOrDefault(s => url.EndsWith(s, StringComparison.OrdinalIgnoreCase) || url.EndsWith($"dashboard/{s}", StringComparison.OrdinalIgnoreCase));
-        if (setDefault && result == null)
-            result = Profile;
-        return result;
-    }
+    UserManager
 }

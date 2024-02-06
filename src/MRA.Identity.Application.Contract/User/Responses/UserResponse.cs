a2fb9e5a0eab12b308c956a1f626bd88a22a5abb
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-02-06 10:44:05 +0500

    update version

--- src/MRA.Identity.Application.Contract/User/Responses/UserResponse.cs
+++ src/MRA.Identity.Application.Contract/User/Responses/UserResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MRA.Identity.Application.Contract.User.Responses;
+﻿namespace MRA.Identity.Application.Contract.User.Responses;
 
 public record class UserResponse
 {

62db579a958e48a16ce90ecf539465d224472806
firuz2006 <firuzisoboev@gmail.com>
2024-02-21 14:02:49 +0500

    auth button in swagger

--- src/MRA.Identity.Api/Program.cs
+++ src/MRA.Identity.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using MRA.Identity.Application;
 using MRA.Identity.Infrastructure;
 using MRA.Identity.Infrastructure.Persistence;
@@ -5,6 +6,7 @@ using MRA.Identity.Api.Filters;
 using FluentValidation;
 using MRA.Identity.Application.Contract.Skills.Command;
 using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
 using MRA.Configurations.Initializer.Azure.Insight;
 using MRA.Configurations.Initializer.Azure.KeyVault;
 using MRA.Configurations.Initializer.Azure.AppConfig;
@@ -19,14 +21,42 @@ if (builder.Environment.IsProduction())
     builder.Configuration.AddAzureAppConfig(appConfigConnectionString);
     builder.Logging.AddApiApplicationInsights(builder.Configuration);
 }
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<ApiExceptionFilterAttribute>();
-});
+
+builder.Services.AddControllers(options => { options.Filters.Add<ApiExceptionFilterAttribute>(); });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo());
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
 
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
@@ -45,7 +75,8 @@ using (var scope = app.Services.CreateScope())
     await initializer.InitialiseAsync();
 }
 
-var applicationDbContextInitializer = app.Services.CreateAsyncScope().ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
+var applicationDbContextInitializer = app.Services.CreateAsyncScope().ServiceProvider
+    .GetRequiredService<ApplicationDbContextInitializer>();
 await applicationDbContextInitializer.SeedAsync();
 
 app.UseCors("CORS_POLICY");

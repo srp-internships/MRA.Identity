56d69e1e756ca1fa8923bbb73af1dec6cc23b65a
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-03-01 14:57:46 +0500

    add migration

---
+++ src/MRA.Identity.Infrastructure/Migrations/20240301095146_UserSkills.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MRA.Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserSkills : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserSkill_AspNetUsers_UserId",
+                table: "UserSkill");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserSkill_Skills_SkillId",
+                table: "UserSkill");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSkill",
+                table: "UserSkill");
+
+            migrationBuilder.RenameTable(
+                name: "UserSkill",
+                newName: "UserSkills");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserSkill_SkillId",
+                table: "UserSkills",
+                newName: "IX_UserSkills_SkillId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSkills",
+                table: "UserSkills",
+                columns: new[] { "UserId", "SkillId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserSkills_AspNetUsers_UserId",
+                table: "UserSkills",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserSkills_Skills_SkillId",
+                table: "UserSkills",
+                column: "SkillId",
+                principalTable: "Skills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserSkills_AspNetUsers_UserId",
+                table: "UserSkills");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserSkills_Skills_SkillId",
+                table: "UserSkills");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSkills",
+                table: "UserSkills");
+
+            migrationBuilder.RenameTable(
+                name: "UserSkills",
+                newName: "UserSkill");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserSkills_SkillId",
+                table: "UserSkill",
+                newName: "IX_UserSkill_SkillId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSkill",
+                table: "UserSkill",
+                columns: new[] { "UserId", "SkillId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserSkill_AspNetUsers_UserId",
+                table: "UserSkill",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserSkill_Skills_SkillId",
+                table: "UserSkill",
+                column: "SkillId",
+                principalTable: "Skills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

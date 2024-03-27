9181ae1bc563bfc2f8a56789711fa74cc5d59e0a
firuz2006 <firuzisoboev@gmail.com>
2024-03-27 14:13:36 +0500

    create entities

---
+++ src/MRA.Identity.Domain/Entities/Application.cs
@@ -0,0 +1,12 @@
+﻿namespace MRA.Identity.Domain.Entities;
+
+public class Application
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public string Description { get; set; }
+    public required string ClientSecret { get; set; }
+    public string[] CallbackUrls { get; set; }
+    public bool IsProtected { get; set; }
+    public ICollection<ApplicationUserLink> ApplicationUserLinks { get; set; }
+}

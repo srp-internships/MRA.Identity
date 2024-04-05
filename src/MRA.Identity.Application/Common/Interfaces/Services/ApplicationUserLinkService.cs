c8f36888f33f5d12d9a1d1ce0a1be5e5a0bf90b2
firuz2006 <firuzisoboev@gmail.com>
2024-04-05 16:34:56 +0500

    finish ApplicationUserLinkService.cs

--- src/MRA.Identity.Application/Common/Interfaces/Services/ApplicationUserLinkService.cs
+++ src/MRA.Identity.Application/Common/Interfaces/Services/ApplicationUserLinkService.cs
@@ -2,6 +2,9 @@
 
 public interface IApplicationUserLinkService
 {
-    public Task CreateUserLinkAsync(Guid userId, Guid applicationId, string callback,
+    public Task<Domain.Entities.Application> CreateUserLinkAsync(Guid userId, Guid applicationId, string callback,
         CancellationToken cancellationToken);
+
+    public Task CreateUserLinkIfNotExistAsync(Guid userId, Guid applicationId,
+        string callback, CancellationToken cancellationToken);
 }

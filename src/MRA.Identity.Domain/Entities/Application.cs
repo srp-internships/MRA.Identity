73d62b9f5a473119bdd5d4887f484ec803db61b1
firuz2006 <firuzisoboev@gmail.com>
2024-04-12 15:22:54 +0500

    fix warnings

--- src/MRA.Identity.Domain/Entities/Application.cs
+++ src/MRA.Identity.Domain/Entities/Application.cs
@@ -8,9 +8,9 @@ public class Application
     public required string Name { get; set; }
     public string Description { get; set; } = "";
     public required string ClientSecret { get; set; }
-    public string[] CallbackUrls { get; set; }
+    public string[] CallbackUrls { get; set; } = [];
     public bool IsProtected { get; set; }
-    public ICollection<ApplicationUserLink> ApplicationUserLinks { get; set; }
+    public ICollection<ApplicationUserLink>? ApplicationUserLinks { get; set; }
     public Guid DefaultRoleId { get; set; }
     public ApplicationRole? DefaultRole { get; set; }
 }

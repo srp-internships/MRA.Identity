e4f115f37885db6097646f3cab338604fea17a12
Muhammadislom Ismatov <108611175+Muhammadislom99@users.noreply.github.com>
2024-04-15 18:54:21 +0500

    refactor

--- src/MRA.Identity.Application/Features/UserProfiles/Query/GetProfileQueryHandler.cs
+++ src/MRA.Identity.Application/Features/UserProfiles/Query/GetProfileQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using MRA.Identity.Application.Common.Exceptions;
@@ -6,6 +7,7 @@ using MRA.Identity.Application.Common.Interfaces.DbContexts;
 using MRA.Identity.Application.Common.Interfaces.Services;
 using MRA.Identity.Application.Contract.Profile.Queries;
 using MRA.Identity.Application.Contract.Profile.Responses;
+using MRA.Identity.Domain.Entities;
 
 namespace MRA.Identity.Application.Features.UserProfiles.Query;
 
@@ -17,35 +19,44 @@ public class GetProfileQueryHandler(
 {
     public async Task<UserProfileResponse> Handle(GetProfileQuery request, CancellationToken cancellationToken)
     {
+        var userName = request.UserName ?? userHttpContextAccessor.GetUserName();
         var userRoles = userHttpContextAccessor.GetUserRoles();
-        var userName = userHttpContextAccessor.GetUserName();
-
-        if (request.UserName != null)
-            userName = request.UserName;
-
-        var user = context.Users
-            .Where(u => u.UserName == userName);
-
 
         if (request.UserName != null && !userRoles.Any())
             throw new ForbiddenAccessException("Access is denied");
-        
-        var application = await context.Applications.FirstOrDefaultAsync(x =>
-                x.Id == request.ApplicationId && x.ClientSecret == request.ApplicationClientSecret,
-            cancellationToken);
 
-        if (!userRoles.Any(r => r == "SuperAdmin") && request.UserName != null)
+        var user = GetUserQuery(context.Users, u => u.UserName == userName);
+
+        if (userHttpContextAccessor.GetUserName() != "SuperAdmin" & request.UserName != null)
         {
-            if (application is null)
-                throw new NotFoundException("There is no such application");
-
-            user = user.Include(u => u.ApplicationUserLinks)
-                .Where(u => u.ApplicationUserLinks.Any(l => l.ApplicationId == application.Id));
+            var application =
+                await GetApplication(request.ApplicationId, request.ApplicationClientSecret, cancellationToken);
+            user = GetUserQuery(user.Include(u => u.ApplicationUserLinks),
+                u => u.ApplicationUserLinks.Any(l => l.ApplicationId == application.Id));
         }
 
         var _user = user.FirstOrDefault();
         if (_user == null) throw new NotFoundException("user is not found");
         var response = mapper.Map<UserProfileResponse>(_user);
         return response;
+    }
+
+    private IQueryable<ApplicationUser> GetUserQuery(IQueryable<ApplicationUser> users,
+        Expression<Func<ApplicationUser, bool>> predicate)
+    {
+        return users.Where(predicate);
+    }
+
+    private async Task<Domain.Entities.Application> GetApplication(Guid applicationId, string applicationClientSecret,
+        CancellationToken cancellationToken)
+    {
+        var application = await context.Applications.FirstOrDefaultAsync(x =>
+                x.Id == applicationId && x.ClientSecret == applicationClientSecret,
+            cancellationToken);
+
+        if (application is null)
+            throw new NotFoundException("There is no such application");
+
+        return application;
     }
 }
--- src/MRA.Identity.Application/Features/Users/Query/GetUserByKeyHandler.cs
+++ src/MRA.Identity.Application/Features/Users/Query/GetUserByKeyHandler.cs
@@ -1,9 +1,11 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MRA.Identity.Application.Common.Exceptions;
 using MRA.Identity.Application.Common.Interfaces.DbContexts;
+using MRA.Identity.Application.Common.Interfaces.Services;
 using MRA.Identity.Application.Contract.User.Queries;
 using MRA.Identity.Application.Contract.User.Responses;
 using MRA.Identity.Domain.Entities;
@@ -13,35 +15,28 @@ namespace MRA.Identity.Application.Features.Users.Query;
 public class GetUserByKeyHandler(
     UserManager<ApplicationUser> userManager,
     IApplicationDbContext dbContext,
-    IMapper mapper)
+    IMapper mapper,
+    IUserHttpContextAccessor httpContextAccessor)
     : IRequestHandler<GetUserByKeyQuery, UserResponse>
 {
     public async Task<UserResponse> Handle(GetUserByKeyQuery request, CancellationToken cancellationToken)
     {
-        var application = await dbContext.Applications.FirstOrDefaultAsync(x =>
-                x.Id == request.ApplicationId && x.ClientSecret == request.ApplicationClientSecret,
-            cancellationToken);
-
-        if (application is null)
-            throw new NotFoundException("There is no such application");
-        
         ApplicationUser user;
-        if (Guid.TryParse(request.Key, out var userId))
-        {
-            user = await userManager.Users
-                .Include(x => x.ApplicationUserLinks)
-                .FirstOrDefaultAsync(
-                    u => u.Id == userId && u.ApplicationUserLinks.Any(l => l.ApplicationId == application.Id),
-                    cancellationToken: cancellationToken);
-        }
+        var isSuperAdmin = httpContextAccessor.GetUserName() == "SuperAdmin";
+        var isGuid = Guid.TryParse(request.Key, out var userId);
+
+        if (isSuperAdmin)
+            user = await GetUserByCondition(u => isGuid ? u.Id == userId : u.UserName == request.Key,
+                cancellationToken);
         else
         {
-            user = await userManager.Users
-                .Include(u => u.ApplicationUserLinks)
-                .FirstOrDefaultAsync(
-                    u => u.UserName == request.Key &&
-                         u.ApplicationUserLinks.Any(l => l.ApplicationId == application.Id),
-                    cancellationToken: cancellationToken);
+            var application =
+                await GetApplication(request.ApplicationId, request.ApplicationClientSecret, cancellationToken);
+
+            user = await GetUserByCondition(u => (isGuid ? u.Id == userId : u.UserName == request.Key) &&
+                                                 u.ApplicationUserLinks.Any(l => l.ApplicationId == application.Id),
+                cancellationToken,
+                true);
         }
 
         if (user == null)
@@ -49,5 +44,28 @@ public class GetUserByKeyHandler(
 
         var result = mapper.Map<UserResponse>(user);
         return result;
+    }
+
+    private async Task<ApplicationUser> GetUserByCondition(Expression<Func<ApplicationUser, bool>> condition,
+        CancellationToken cancellationToken, bool includeLinks = false)
+    {
+        var users = userManager.Users;
+        if (includeLinks)
+            users = users.Include(u => u.ApplicationUserLinks);
+
+        return await users.FirstOrDefaultAsync(condition, cancellationToken);
+    }
+
+    private async Task<Domain.Entities.Application> GetApplication(Guid applicationId, string applicationClientSecret,
+        CancellationToken cancellationToken)
+    {
+        var application = await dbContext.Applications.FirstOrDefaultAsync(x =>
+                x.Id == applicationId && x.ClientSecret == applicationClientSecret,
+            cancellationToken);
+
+        if (application is null)
+            throw new NotFoundException("There is no such application");
+
+        return application;
     }
 }

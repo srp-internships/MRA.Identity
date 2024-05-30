28e1de6193ab4a50d70541f4736278538793d078
firuz2006 <firuzisoboev@gmail.com>
2024-05-30 09:23:25 +0500

    fix reset password

--- src/MRA.Identity.Client/Services/Auth/IAuthService.cs
+++ src/MRA.Identity.Client/Services/Auth/IAuthService.cs
@@ -20,4 +20,6 @@ public interface IAuthService
     Task ResendVerificationEmail();
     Task<ApiResponse> SendVerificationEmailToken(string token, string userId);
     Task NavigateToCallbackWithJwt(string callback);
+    string GetDefaultSSOAppId();
+    string GetDefaultSSOAppCallback();
 }
